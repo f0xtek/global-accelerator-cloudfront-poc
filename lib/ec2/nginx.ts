deb0e776483dad21fd9254b98f6cbc900981c3e9
Luke Anderson <landerson61@gmail.com>
2023-03-26 09:23:19 +0100

    Manage EC2 IAM role in code
    Management of EC2 role allowsd for creation of instance profile and ability to add managed SSM policy for SSM console access.

--- lib/ec2/nginx.ts
+++ lib/ec2/nginx.ts
@@ -1,3 +1,4 @@
+import { RemovalPolicy } from "aws-cdk-lib";
 import {
   AmazonLinuxGeneration,
   AmazonLinuxImage,
@@ -5,9 +6,16 @@ import {
   InstanceClass,
   InstanceSize,
   InstanceType,
+  SecurityGroup,
   SubnetType,
   UserData,
 } from "aws-cdk-lib/aws-ec2";
+import {
+  CfnInstanceProfile,
+  ManagedPolicy,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 import { Network } from "../network/network";
 
@@ -22,13 +30,36 @@ userData.addCommands(
 export interface NginxInstaceProps {
   namePrefix: string;
   network: Network;
+  securityGroup: SecurityGroup;
 }
 
 export class NginxInstance extends Construct {
   instance: Instance;
+  instanceProfile: CfnInstanceProfile;
 
   constructor(scope: Construct, id: string, props: NginxInstaceProps) {
     super(scope, id);
+
+    const roleName = `nginxInstance-${props.namePrefix}`;
+
+    const policy = ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "SSM",
+      "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    );
+
+    const role: Role = new Role(this, `nginxInstance-${props.namePrefix}`, {
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+      managedPolicies: [policy],
+      roleName,
+    });
+    role.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+    this.instanceProfile = new CfnInstanceProfile(this, "SSMInstanceProfile", {
+      instanceProfileName: roleName,
+      roles: [role.roleName],
+    });
+    this.instanceProfile.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
     this.instance = new Instance(scope, `nginxInstance-${props.namePrefix}`, {
       instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
@@ -40,10 +71,13 @@ export class NginxInstance extends Construct {
         subnetType: SubnetType.PUBLIC,
       },
       instanceName: `${props.namePrefix}-nginx`,
-      userData: userData,
+      role,
+      userData,
       requireImdsv2: true,
       userDataCausesReplacement: true,
+      securityGroup: props.securityGroup,
     });
+    this.instance.applyRemovalPolicy(RemovalPolicy.DESTROY);
   }
 
   get publicDnsName() {
