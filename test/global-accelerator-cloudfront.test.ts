bc771bc7f8e2a1faf999513184bf531d4aa11fff
Luke Anderson <landerson61@gmail.com>
2023-03-27 17:33:36 +0100

    Add some initial tests

--- test/global-accelerator-cloudfront.test.ts
+++ test/global-accelerator-cloudfront.test.ts
@@ -1,16 +1,40 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as GlobalAcceleratorCloudfront from '../lib/global-accelerator-cloudfront-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as GlobalAcceleratorCloudfront from "../lib/global-accelerator-cloudfront-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/global-accelerator-cloudfront-stack.ts
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new GlobalAcceleratorCloudfront.GlobalAcceleratorCloudfrontStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+const app = new cdk.App();
+const stack = new GlobalAcceleratorCloudfront.GlobalAcceleratorCloudfrontStack(
+  app,
+  "MyTestStack"
+);
+const template = Template.fromStack(stack);
+
+test("VPC Created", () => {
+  // const app = new cdk.App();
+  // WHEN
+  // const stack =
+  //   new GlobalAcceleratorCloudfront.GlobalAcceleratorCloudfrontStack(
+  //     app,
+  //     "MyTestStack"
+  //   );
+  // THEN
+  // const template = Template.fromStack(stack);
+  template.hasResourceProperties("AWS::EC2::VPC", {
+    EnableDnsHostnames: true,
+    EnableDnsSupport: true,
+    CidrBlock: "10.0.0.0/16",
+  });
+});
+
+test("All Subnets Created", () => {
+  // const app = new cdk.App();
+  // WHEN
+  // const stack =
+  //   new GlobalAcceleratorCloudfront.GlobalAcceleratorCloudfrontStack(
+  //     app,
+  //     "MyTestStack"
+  //   );
+  // THEN
+  // const template = Template.fromStack(stack);
+  template.resourceCountIs("AWS::EC2::Subnet", 4);
 });
